eff80868461daa20e5c289509e3a2fada030b6b1
astromech_droid <n.yamamoto.py@gmail.com>
2023-02-05 13:44:54 +0900

    vttをダウンロードする機能を実装完了

---
+++ main.py
@@ -0,0 +1,5 @@
+from lib import vtt
+
+url = "https://ipv4-c003-itm001-k-opticom-isp.1.oca.nflxvideo.net/?o=1&v=121&e=1675609223&t=fr-uZkiRvIUukvSmUZ7cohOOE154ZzZvoglYsKlh5bcx5GNRrPRLiK2KVGPEaZR7Zo_VGlh--TxZ9x_AgHTwTTfuXo7rt5_zc8Ju84QrJdbfAmUDADtMA4l46jBcwWltfIrgF7Mhj_d4E0XerDCfXeDK-LDuQaAsRJSOEKtHhdejm9GICuzDBwy2m2F2qJDvKFkGbGDxG5ddZydAEeBUuIGwk4Y47pYeA-hRkfZZ-zFnHw"
+path = "data/breaking_bad_s1_e1.vtt"
+vtt.download(url, path)
---
+++ tests/test_vtt.py
@@ -0,0 +1,21 @@
+from lines.lib import vtt
+
+# Breaking Bad s1 e1 on Netflix
+url = "https://ipv4-c003-itm001-k-opticom-isp.1.oca.nflxvideo.net/?o=1&v=121&e=1675609223&t=fr-uZkiRvIUukvSmUZ7cohOOE154ZzZvoglYsKlh5bcx5GNRrPRLiK2KVGPEaZR7Zo_VGlh--TxZ9x_AgHTwTTfuXo7rt5_zc8Ju84QrJdbfAmUDADtMA4l46jBcwWltfIrgF7Mhj_d4E0XerDCfXeDK-LDuQaAsRJSOEKtHhdejm9GICuzDBwy2m2F2qJDvKFkGbGDxG5ddZydAEeBUuIGwk4Y47pYeA-hRkfZZ-zFnHw"
+
+
+def test_fetch():
+    assert type(vtt.fetch(url)) == str
+    assert vtt.fetch("https://google.com") is None
+
+
+def test_save(tmp_path):
+    text = "test_save"
+    path = tmp_path / "test_save.txt"
+
+    assert vtt.save(text, path) is True
+
+
+def test_download(tmp_path):
+    path = tmp_path / "test_download.txt"
+    assert vtt.download(url, path) is True
